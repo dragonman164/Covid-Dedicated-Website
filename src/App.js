dc013a34a36b86f556a86ba56db9aba24cedf03f
dragonman164 <sanidhiyafirefox123@gmail.com>
2021-04-28 19:43:56 +0530

    Covid Dedicated Website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/NavBar';
+import Footer from './Components/Footer';
+import {Switch,Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import Resources from './Pages/Resources';
+import Testing from './Pages/Testing';
+import Tracker from './Pages/Tracker';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Navbar/>
+   <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/testing" component={Testing}/>
+    <Route exact path="/track" component={Tracker}/>
+    <Route exact path="/resources" component={Resources}/>
+   </Switch>
+   <Footer/>
+   </>
   );
 }
 
